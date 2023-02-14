4777943f97e882c0af64c882bb3425198e77f47d
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2023-02-14 14:03:33 +0100

    Add an example of lazy load – Bootstrap tabs

--- src/js/components/accordion.js
+++
@@ -1,3 +0,0 @@
-export default els => {
-    // TODO
-};
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { loadForElements } from './helpers/module-loader';
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadForElements(document.querySelectorAll('[data-accordion]'), () => import('./components/accordion'));
+    loadForElements(document.querySelectorAll('[data-bs-toggle]'), () => import('bootstrap/js/src/tab'));
 });

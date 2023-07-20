76e625380a9414c47925b9109db1857ccb50ef60
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2023-07-20 13:31:31 +0200

    Fix generating the icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,155 @@
+const fs = require('fs');
+const md5Dir = require('md5-dir');
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const iconFilesHash = md5Dir.sync(dir('src/icons')).substring(0, 20);
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
-function configName(name, config) {
-    config.name = name;
-    return config;
+// Remove the query string form the keys of manifest.json file, especially those for IcoMoon font icons
+class RemoveQueryStringFromManifestPlugin {
+    constructor(outputPath) {
+        this.outputPath = outputPath;
+    }
+
+    apply(compiler) {
+        compiler.hooks.done.tap('RemoveQueryStringFromManifestPlugin', () => {
+            const path = dir(`${this.outputPath}/manifest.json`);
+            const manifest = JSON.parse(fs.readFileSync(path));
+
+            Object.keys(manifest)
+                .filter(key => key.includes('?'))
+                .forEach(key => {
+                    manifest[key.split('?').shift()] = manifest[key];
+                    delete manifest[key];
+                });
+
+            fs.writeFileSync(path, JSON.stringify(manifest));
+        });
+    }
+}
+
+function dir(value) {
+    return path.resolve(__dirname, value);
+}
+
+function getFilename(folder, hash = '[contenthash]') {
+    return Encore.isProduction() ? `${folder}/[name].${hash}[ext]` : `${folder}/[name][ext]`;
+}
+
+function createConfigDraft(outputPath, publicPath) {
+    Encore.reset();
+
+    return Encore
+        .addEntry('open-frontend', './src/entrypoint.js')
+        .addPlugin(new RemoveQueryStringFromManifestPlugin(outputPath))
+        .cleanupOutputBeforeBuild()
+        .disableSingleRuntimeChunk()
+        .enableSassLoader()
+        .setManifestKeyPrefix('')
+        .setOutputPath(outputPath)
+        .setPublicPath(publicPath)
+
+        // Load the fonts and icons
+        .configureFontRule({ enabled: false })
+        .addLoader({
+            test: /\.(woff|woff2|ttf|eot|otf)$/i,
+            include: [
+                dir('src/fonts'),
+                dir('node_modules/@fontsource'),
+            ],
+            oneOf: [
+                {
+                    resourceQuery: /copy-files-loader/,
+                    type: 'javascript/auto',
+                },
+                {
+                    type: 'asset/resource',
+                    generator: {
+                        filename: getFilename('fonts'),
+                    },
+                },
+            ],
+        })
+        .addLoader({
+            test: /\.(woff|woff2|ttf|eot|otf|svg)$/i,
+            include: [
+                dir('src/icons'),
+            ],
+            oneOf: [
+                {
+                    resourceQuery: /copy-files-loader/,
+                    type: 'javascript/auto',
+                },
+                {
+                    type: 'asset/resource',
+                    generator: {
+                        filename: getFilename('icons', iconFilesHash)
+                    },
+                },
+            ],
+        })
+
+        // Load optimized and minified images
+        .configureImageRule({ enabled: false })
+        .addLoader({
+            test: /\.(png|jpg|jpeg)$/i,
+            oneOf: [
+                {
+                    resourceQuery: /copy-files-loader/,
+                    type: 'javascript/auto',
+                },
+                {
+                    type: 'asset/resource',
+                    generator: {
+                        filename: getFilename('images'),
+                    },
+                },
+            ],
+        })
+        .addLoader({
+            test: /\.(gif|ico|svg|webp|avif)$/i,
+            exclude: [
+                dir('src/fonts'),
+                dir('src/icons'),
+            ],
+            oneOf: [
+                {
+                    resourceQuery: /copy-files-loader/,
+                    type: 'javascript/auto',
+                },
+                {
+                    type: 'asset/resource',
+                    generator: {
+                        filename: getFilename('images'),
+                    },
+                },
+            ],
+        })
+    ;
 }
 
 function createDistConfig() {
-    Encore.reset();
-
-    return Encore
-        .setOutputPath('dist')
-        .setPublicPath('/')
-        .setManifestKeyPrefix('')
-        .addEntry('open-frontend', './src/entrypoint.js')
-        .enableSassLoader()
-        .disableSingleRuntimeChunk()
-        .splitEntryChunks()
-        .cleanupOutputBeforeBuild()
-        .getWebpackConfig()
-    ;
+    return createConfigDraft('dist', '/')
+        .getWebpackConfig();
 }
 
 function createDocsConfig() {
-    Encore.reset();
-
-    return Encore
-        .setOutputPath('_site-dist')
-        .setPublicPath('./dist')
-        .setManifestKeyPrefix('')
-        .addEntry('open-frontend', './src/entrypoint.js')
-        .enableSassLoader()
+    return createConfigDraft('docs', '_site-dist', './dist')
+        .enableSourceMaps()
         .enableVersioning(Encore.isProduction())
-        .disableSingleRuntimeChunk()
-        .splitEntryChunks()
-        .cleanupOutputBeforeBuild()
-        .getWebpackConfig()
-    ;
+        .getWebpackConfig();
+}
+
+function createNamedConfig(name, config) {
+    config.name = name;
+
+    return config;
 }
 
 module.exports = [
-    configName('dist', createDistConfig()),
-    configName('docs', createDocsConfig()),
+    createNamedConfig('dist', createDistConfig()),
+    createNamedConfig('docs', createDocsConfig()),
 ];

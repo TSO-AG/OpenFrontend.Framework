1f89a4a003f6f01718eec198e54d6031d6d64855
Miłosz Bauman <kontakt@baudesign.pl>
2023-09-11 10:03:56 +0200

    Add a play/pause button to the video

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   loadForElements(document.querySelectorAll('[data-of-datepicker]'), () => import(/* webpackChunkName: "open-frontend-datepicker" */ './components/datepicker'))
   loadForElements(document.querySelectorAll('[data-of-table-sort]'), () => import(/* webpackChunkName: "open-frontend-tablesort" */ './components/tablesort'))
   loadForElements(document.querySelectorAll('[data-of-rating]'), () => import(/* webpackChunkName: "open-frontend-rating" */ './components/rating'))
+  loadForElements(document.querySelectorAll('[data-of-video-play]'), () => import(/* webpackChunkName: "open-frontend-video-play" */ './components/video-play'))
 })
 
 window.bootstrap = {

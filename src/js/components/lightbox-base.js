3c6149ef05f07590a033fe5b8d57a67190a18867
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2023-12-15 14:19:57 +0100

    JS Lint

--- src/js/components/lightbox-base.js
+++ src/js/components/lightbox-base.js
@@ -12,11 +12,11 @@ const LIGHTBOX_WRAPPER_CLASS = 'lightbox-wrapper'
 const LIGHTBOX_THUMBNAILS_CLASS = 'lightbox-thumbnails'
 
 const LIGHTBOX_SETTINGS = {
-    autoplayVideos: true,
-    closeEffect: 'fade',
-    closeOnOutsideClick: false,
-    openEffect: 'fade',
-    touchNavigation: true,
+  autoplayVideos: true,
+  closeEffect: 'fade',
+  closeOnOutsideClick: false,
+  openEffect: 'fade',
+  touchNavigation: true,
 }
 
 const LIGHTBOX_HTML = `<div id="glightbox-body" class="glightbox-container" tabindex="-1" role="dialog" aria-hidden="false">
@@ -49,239 +49,237 @@ const THUMBNAILS_CAROUSEL_HTML = `<div class="gallery-thumbnails carousel carous
 </div>`
 
 const THUMBNAILS_CAROUSEL_SETTINGS = {
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    navigation: {
-        nextEl: '.carousel-button-next',
-        prevEl: '.carousel-button-prev',
-    },
-    slideToClickedSlide: true,
-    watchOverflow: true,
-    centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.carousel-button-next',
+    prevEl: '.carousel-button-prev',
+  },
+  slideToClickedSlide: true,
+  watchOverflow: true,
+  centeredSlides: true,
 }
 
 const DefaultType = {
-    items: 'array',
-    tabs: 'array',
-    thumbnails: 'boolean',
+  items: 'array',
+  tabs: 'array',
+  thumbnails: 'boolean',
 }
 
 const Default = {
-    items: [],
-    tabs: [],
-    thumbnails: false,
+  items: [],
+  tabs: [],
+  thumbnails: false,
 }
 
-export class Lightbox extends Config {
-    constructor(identifier, config) {
-        super()
+class Lightbox extends Config {
+  constructor(identifier, config) {
+    super()
 
-        this._config = this._getConfig(config)
-        this._identifier = identifier
+    this._config = this._getConfig(config)
+    this._identifier = identifier
 
-        this._lightbox = new GLightbox(this._getLightboxOptions())
-        this._lightbox.on('close', this._onClose.bind(this))
-        this._lightbox.on('open', this._onOpen.bind(this))
-        this._lightbox.on('slide_before_change', this._onSlideBeforeChange.bind(this))
-        this._lightbox.on('slide_changed', this._onSlideChanged.bind(this))
+    this._lightbox = new GLightbox(this._getLightboxOptions())
+    this._lightbox.on('close', this._onClose.bind(this))
+    this._lightbox.on('open', this._onOpen.bind(this))
+    this._lightbox.on('slide_before_change', this._onSlideBeforeChange.bind(this))
+    this._lightbox.on('slide_changed', this._onSlideChanged.bind(this))
+  }
+
+  // Getters
+  static get Default() {
+    return Default
+  }
+
+  static get DefaultType() {
+    return DefaultType
+  }
+
+  static get NAME() {
+    return NAME
+  }
+
+  // Public
+  open(identifierOrIndex) {
+    let index
+
+    // Find the item with given identifier
+    if (typeof identifierOrIndex === 'string' && /^\d+$/.exec(identifierOrIndex) === null) {
+      index = this._config.items.findIndex(item => item.identifier === identifierOrIndex)
+      index = (index === -1) ? 0 : index
+    } else {
+      index = identifierOrIndex || 0
     }
 
-    // Getters
-    static get Default() {
-        return Default
+    this._lightbox.openAt(index)
+
+    if (this._config.thumbnails) {
+      this._setThumbnailsActiveSlide(index, 0)
+    }
+  }
+
+  // Private
+  _getLightboxOptions() {
+    let html = LIGHTBOX_HTML
+    html = html.replace(/{beforeContent}/, this._getBeforeContentHtml())
+    html = html.replace(/{afterContent}/, this._getAfterContentHtml())
+
+    return {
+      ...LIGHTBOX_SETTINGS,
+      elements: this._config.items.map(item => ({ ...item, zoomable: false })),
+      lightboxHTML: html,
+    }
+  }
+
+  _getBeforeContentHtml() {
+    return ''
+  }
+
+  _getAfterContentHtml() {
+    return this._config.thumbnails ? `<div class="${LIGHTBOX_THUMBNAILS_CLASS}"></div>` : ''
+  }
+
+  _onClose() {
+    if (this._config.thumbnails) {
+      this._thumbnailsElement.remove()
+      this._thumbnailsElement = null
+      this._thumbnailsCarousel = null
+    }
+  }
+
+  _onOpen() {
+    if (this._config.thumbnails) {
+      this._thumbnailsElement = this._getLightboxElement(`.${LIGHTBOX_THUMBNAILS_CLASS}`)
+      this._createResizeObserver(this._thumbnailsElement, 'thumbnails-wrapper-height')
+
+      this._generateThumbnails()
+      this._updateThumbnails()
+    }
+  }
+
+  _onSlideBeforeChange({ current }) {
+    if (this._config.thumbnails) {
+      this._setThumbnailsActiveSlide(current.index)
+    }
+  }
+
+  _onSlideChanged({ current }) {
+    if (this._descriptionObserver && this._currentDescription) {
+      this._descriptionObserver.unobserve(this._currentDescription)
     }
 
-    static get DefaultType() {
-        return DefaultType
+    this._currentDescription = current?.slideNode?.querySelector('.gslide-description')
+
+    // After changing the slide, set the maximum image height to fit in the window
+    if (this._currentDescription && this._currentDescription.textContent && window.getComputedStyle(this._currentDescription).position !== 'absolute') {
+      this._descriptionObserver = this._createResizeObserver(this._currentDescription, 'description-height')
+    } else {
+      document.documentElement.style.setProperty('--lightbox-description-height', '0px')
+    }
+  }
+
+  _getLightboxElement(selector) {
+    return this._lightbox.modal.querySelector(selector)
+  }
+
+  _generateThumbnails() {
+    const temp = document.createElement('div')
+    temp.innerHTML = THUMBNAILS_CAROUSEL_HTML
+
+    this._thumbnailsElement.append(temp.firstElementChild)
+    this._thumbnailsCarousel = new Swiper(this._thumbnailsElement.querySelector('.swiper'), THUMBNAILS_CAROUSEL_SETTINGS)
+
+    // Change the lightbox slide when the carousel slide has changed and they are different
+    this._thumbnailsCarousel.on('slideChange', () => {
+      if (this._thumbnailsCarouselLocked) {
+        return
+      }
+
+      this._setThumbnailsActiveSlide(this._thumbnailsCarousel.realIndex)
+    })
+  }
+
+  _updateThumbnails() {
+    if (!this._thumbnailsCarousel) {
+      return
     }
 
-    static get NAME() {
-        return NAME
-    }
+    const imageRegExp = /\.(gif|jpg|jpeg|png|svg|webp)$/
 
-    // Public
-    open(identifierOrIndex) {
-        let index;
+    const generator = element => {
+      const slide = document.createElement('div')
+      slide.className = 'swiper-slide'
 
-        // Find the item with given identifier
-        if (typeof identifierOrIndex === 'string' && /^\d+$/.exec(identifierOrIndex) === null) {
-            index = this._config.items.findIndex(item => item.identifier === identifierOrIndex)
-            index = (index !== -1) ? index : 0
-        } else {
-            index = identifierOrIndex || 0
+      let { thumbnail } = element
+
+      // Try to detect the image, if type is unknown
+      if (!thumbnail && !element.type && element.href && imageRegExp.test(element.href)) {
+        element.type = 'image'
+      }
+
+      // Set the original image as thumbnail
+      if (!thumbnail && element.type === 'image') {
+        thumbnail = element.href
+      }
+
+      if (thumbnail) {
+        const image = document.createElement('img')
+        image.src = thumbnail
+        image.alt = element.alt || ''
+
+        if (element.thumbnailSrcset) {
+          image.srcset = element.thumbnailSrcset
+
+          if (element.thumbnailSizes) {
+            image.sizes = element.thumbnailSizes
+          }
         }
 
-        this._lightbox.openAt(index)
+        slide.append(image)
+      }
 
-        if (this._config.thumbnails) {
-            this._setThumbnailsActiveSlide(index, 0)
-        }
+      return slide
     }
 
-    // Private
-    _getLightboxOptions() {
-        let html = LIGHTBOX_HTML;
-        html = html.replace(/{beforeContent}/, this._getBeforeContentHtml())
-        html = html.replace(/{afterContent}/, this._getAfterContentHtml())
+    // Disable the carousel and remove all existing slides
+    this._thumbnailsCarouselLocked = true
+    this._thumbnailsCarousel.disable()
+    this._thumbnailsCarousel.removeAllSlides()
 
-        return {
-            ...LIGHTBOX_SETTINGS,
-            elements: this._config.items.map(item => ({...item, zoomable: false})),
-            lightboxHTML: html,
-        }
+    // Append new slides and re-enable the carousel
+    this._thumbnailsCarousel.appendSlide(this._generateThumbnailsCarouselSlides(generator))
+    this._thumbnailsCarousel.enable()
+    this._thumbnailsCarouselLocked = false
+  }
+
+  _generateThumbnailsCarouselSlides(generator) {
+    return this._config.items.map(generator)
+  }
+
+  _setThumbnailsActiveSlide(index, speed) {
+    if (this._thumbnailsCarousel && this._thumbnailsCarousel.slides.length >= index) {
+      this._thumbnailsCarousel.slideTo(index, speed)
     }
 
-    _getBeforeContentHtml() {
-        return ''
+    // Also change the lightbox slide, if it's not the same
+    if (this._lightbox.index !== index) {
+      this._lightbox.goToSlide(index)
     }
+  }
 
-    _getAfterContentHtml() {
-        return this._config.thumbnails ? `<div class="${LIGHTBOX_THUMBNAILS_CLASS}"></div>` : ''
-    }
+  _createResizeObserver(element, variableName) {
+    // eslint-disable-next-line unicorn/no-array-for-each
+    const resizeObserver = new ResizeObserver(entries => entries.forEach(() => this._updateCssVariable(element, variableName)))
+    resizeObserver.observe(element)
 
-    _onClose() {
-        if (this._config.thumbnails) {
-            this._thumbnailsElement.remove()
-            this._thumbnailsElement = null
-            this._thumbnailsCarousel = null
-        }
-    }
+    this._updateCssVariable(element, variableName)
 
-    _onOpen() {
-        if (this._config.thumbnails) {
-            this._thumbnailsElement = this._getLightboxElement(`.${LIGHTBOX_THUMBNAILS_CLASS}`)
-            this._createResizeObserver(this._thumbnailsElement, 'thumbnails-wrapper-height')
+    return resizeObserver
+  }
 
-            this._generateThumbnails()
-            this._updateThumbnails()
-        }
-    }
-
-    _onSlideBeforeChange({ current }) {
-        if (this._config.thumbnails) {
-            this._setThumbnailsActiveSlide(current.index)
-        }
-    }
-
-    _onSlideChanged({current}) {
-        if (this._descriptionObserver && this._currentDescription) {
-            this._descriptionObserver.unobserve(this._currentDescription)
-        }
-
-        this._currentDescription = current?.slideNode?.querySelector('.gslide-description')
-
-        // After changing the slide, set the maximum image height to fit in the window
-        if (this._currentDescription && this._currentDescription.textContent && window.getComputedStyle(this._currentDescription).position !== 'absolute') {
-            this._descriptionObserver = this._createResizeObserver(this._currentDescription, 'description-height')
-        } else {
-            document.documentElement.style.setProperty('--lightbox-description-height', '0px')
-        }
-    }
-
-    _getLightboxElement(selector) {
-        return this._lightbox.modal.querySelector(selector)
-    }
-
-    _generateThumbnails() {
-        const temp = document.createElement('div')
-        temp.innerHTML = THUMBNAILS_CAROUSEL_HTML
-
-        this._thumbnailsElement.append(temp.firstElementChild)
-        this._thumbnailsCarousel = new Swiper(this._thumbnailsElement.querySelector('.swiper'), THUMBNAILS_CAROUSEL_SETTINGS)
-
-        // Change the lightbox slide when the carousel slide has changed and they are different
-        this._thumbnailsCarousel.on('slideChange', () => {
-            if (this._thumbnailsCarouselLocked) {
-                return
-            }
-
-            this._setThumbnailsActiveSlide(this._thumbnailsCarousel.realIndex)
-        })
-    }
-
-    _updateThumbnails() {
-        if (!this._thumbnailsCarousel) {
-            return
-        }
-
-        const imageRegExp = new RegExp('\.(gif|jpg|jpeg|png|svg|webp)$')
-
-        const generator = element => {
-            const slide = document.createElement('div')
-            slide.className = 'swiper-slide'
-
-            let thumbnail = element.thumbnail
-
-            // Try to detect the image, if type is unknown
-            if (!thumbnail && !element.type && element.href && imageRegExp.test(element.href)) {
-                element.type = 'image'
-            }
-
-            // Set the original image as thumbnail
-            if (!thumbnail && element.type === 'image') {
-                thumbnail = element.href
-            }
-
-            if (thumbnail) {
-                const image = document.createElement('img')
-                image.src = thumbnail
-                image.alt = element.alt || ''
-
-                if (element.thumbnailSrcset) {
-                    image.srcset = thumbnailSrcset
-
-                    if (element.thumbnailSizes) {
-                        image.sizes = thumbnailSizes
-                    }
-                }
-
-                slide.append(image)
-            }
-
-            return slide
-        }
-
-        // Disable the carousel and remove all existing slides
-        this._thumbnailsCarouselLocked = true
-        this._thumbnailsCarousel.disable()
-        this._thumbnailsCarousel.removeAllSlides()
-
-        // Append new slides and re-enable the carousel
-        this._thumbnailsCarousel.appendSlide(this._generateThumbnailsCarouselSlides(generator))
-        this._thumbnailsCarousel.enable()
-        this._thumbnailsCarouselLocked = false
-    }
-
-    _generateThumbnailsCarouselSlides(generator) {
-        return this._config.items.map(generator)
-    }
-
-    _setThumbnailsActiveSlide(index, speed = undefined) {
-        if (this._thumbnailsCarousel && this._thumbnailsCarousel.slides.length >= index) {
-            this._thumbnailsCarousel.slideTo(index, speed)
-        }
-
-        // Also change the lightbox slide, if it's not the same
-        if (this._lightbox.index !== index) {
-            this._lightbox.goToSlide(index)
-        }
-    }
-
-    _createResizeObserver(element, variableName) {
-        const updateCssVariable = (element, variableName) => {
-            console.log(element);
-            console.log(`${element.getBoundingClientRect().height}px`);
-
-            document.documentElement.style.setProperty(`--lightbox-${variableName}`, `${element.getBoundingClientRect().height}px`)
-
-        }
-        const resizeObserver = new ResizeObserver(entries => entries.forEach(() => updateCssVariable(element, variableName)))
-
-        resizeObserver.observe(element)
-        updateCssVariable(element, variableName)
-
-        return resizeObserver
-    }
+  _updateCssVariable(element, variableName) {
+    document.documentElement.style.setProperty(`--lightbox-${variableName}`, `${element.getBoundingClientRect().height}px`)
+  }
 }
 
 export default Lightbox
--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -1,11 +1,10 @@
-import LightboxWithTabs from './lightbox-with-tabs';
-import Lightbox from './lightbox-base';
+import Lightbox from './lightbox-base'
+import LightboxWithTabs from './lightbox-with-tabs'
 
-class LightboxRegistry
-{
+class LightboxRegistry {
   constructor() {
-    this.configs = new Map;
-    this.instances = new Map;
+    this.configs = new Map()
+    this.instances = new Map()
   }
 
   initializeLightboxes() {
@@ -14,8 +13,8 @@ class LightboxRegistry
 
       // Get the HTML elements and add them to items
       if (Object.prototype.hasOwnProperty.call(config, 'elements')) {
-        config.elements.forEach(({ element, options }, index) => {
-          const item = {...options}
+        for (const [index, { element, options }] of config.elements.entries()) {
+          const item = { ...options }
 
           if (element.hasAttribute('href')) {
             item.href = element.href
@@ -27,11 +26,11 @@ class LightboxRegistry
           element.addEventListener('click', e => {
             e.preventDefault()
             this.instances.get(identifier).open(index)
-          });
-        });
+          })
+        }
       }
 
-      config.items = config.items ? [...items, ...config.items] : items;
+      config.items = config.items ? [...items, ...config.items] : items
 
       if (Object.prototype.hasOwnProperty.call(config, 'tabs') && config.tabs.length > 0) {
         this.instances.set(identifier, new LightboxWithTabs(identifier, config))
@@ -42,7 +41,7 @@ class LightboxRegistry
   }
 
   registerFromConfigElement(el) {
-    const identifier = el.dataset.ofLightboxConfig;
+    const identifier = el.dataset.ofLightboxConfig
 
     if (this.configs.has(identifier)) {
       throw new Error(`You cannot have more than one lightbox config with the same identifier: ${identifier}`)
@@ -51,17 +50,17 @@ class LightboxRegistry
     const config = JSON.parse(el.textContent.trim())
     config.elements = config.elements || []
 
-    this.configs.set(identifier, config);
+    this.configs.set(identifier, config)
   }
 
   registerFromRegularElement(el) {
     // TODO: validate options
-    const options = (el.dataset.ofLightbox !== '') ? JSON.parse(el.dataset.ofLightbox) : {}
+    const options = (el.dataset.ofLightbox === '') ? {} : JSON.parse(el.dataset.ofLightbox)
     const identifier = Object.prototype.hasOwnProperty.call(options, 'group') ? options.group : 'ungroupped'
 
     // Create a config if it does not exist yet
     if (!this.configs.has(identifier)) {
-      this.configs.set(identifier, { elements: [] });
+      this.configs.set(identifier, { elements: [] })
     }
 
     this.configs.get(identifier).elements.push({
@@ -74,7 +73,7 @@ class LightboxRegistry
     el.addEventListener('click', e => {
       e.preventDefault()
 
-      const [identifier, item] = el.dataset.ofLightboxOpen.split(':');
+      const [identifier, item] = el.dataset.ofLightboxOpen.split(':')
 
       if (!this.instances.has(identifier)) {
         throw new Error(`The lightbox with identifier "${identifier}" does not exist`)
@@ -85,20 +84,26 @@ class LightboxRegistry
   }
 }
 
-const registry = new LightboxRegistry();
+const registry = new LightboxRegistry()
 
 export function initMultiple(els) {
   // First, register the configs
-  [...els].filter(el => el.hasAttribute('data-of-lightbox-config')).forEach((el) => registry.registerFromConfigElement(el));
+  for (const el of [...els].filter(el => el.hasAttribute('data-of-lightbox-config'))) {
+    registry.registerFromConfigElement(el)
+  }
 
   // Then, register the HTML elements
-  [...els].filter(el => el.hasAttribute('data-of-lightbox')).forEach((el) => registry.registerFromRegularElement(el));
+  for (const el of [...els].filter(el => el.hasAttribute('data-of-lightbox'))) {
+    registry.registerFromRegularElement(el)
+  }
 
   // Then, register the trigger elements
-  [...els].filter(el => el.hasAttribute('data-of-lightbox-open')).forEach((el) => registry.registerTriggerElement(el));
+  for (const el of [...els].filter(el => el.hasAttribute('data-of-lightbox-open'))) {
+    registry.registerTriggerElement(el)
+  }
 
   // Finally initialize the lightboxes
-  registry.initializeLightboxes();
+  registry.initializeLightboxes()
 }
 
 export default LightboxRegistry

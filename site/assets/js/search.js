d8648ee6e7d9d5bd2125c8f6e420ec09e0cd8975
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2024-01-23 17:12:54 +0100

    Fix the Algolia search

--- site/assets/js/search.js
+++ site/assets/js/search.js
@@ -11,8 +11,6 @@
     return
   }
 
-  const siteDocsVersion = searchElement.getAttribute('data-bd-docs-version')
-
   window.docsearch({
     apiKey: '83e0710cd8094cb2e05351f4040d23de',
     indexName: 'openfrontend',
@@ -20,15 +18,6 @@
     container: searchElement,
     transformItems(items) {
       return items.map(item => {
-        const liveUrl = 'https://openfrontend.tourismusweb.site/'
-
-        item.url = window.location.origin.startsWith(liveUrl) ?
-          // On production, return the result as is
-          item.url :
-          // On development or Netlify, replace `item.url` with a trailing slash,
-          // so that the result link is relative to the server root
-          item.url.replace(liveUrl, '/')
-
         // Prevent jumping to first header
         if (item.anchor === 'content') {
           item.url = item.url.replace(/#content$/, '')
